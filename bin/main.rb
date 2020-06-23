7cbde1e0ca68e76e5657617b6c3dc820cc240953
Laguna1 <lagunazoomarket@gmail.com>
2020-06-24 02:52:03 +0300

    fixed linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,5 @@
 # 'Inconsistent indentation detected.'.freeze
+
 require_relative '../lib/scraper5'
 
 class Start < Scraper
@@ -8,43 +9,71 @@ class Start < Scraper
 
   def welcome
     puts
+
     puts
+
     puts
+
     puts '**********************************************************************************************************'
+
     puts '***                                             "Hello!"                                                ***'
+
     puts '***                   "Here you can find out about the main attractions and monuments"                  ***'
+
     puts '***                           "of the beautiful Ukrainian city Kharkiv."                                ***'
+
     puts '***                                       "Please choose y/n"                                           ***'
+
     puts '**********************************************************************************************************'
   end
 
   def user_answer
     loop do
       answer = gets.chomp.downcase
-        if answer == 'y'
-          puts '************************************************************************************************'
-          puts '                                         "Let\'s start!"                                        '
-          puts '************************************************************************************************'
-          puts
-          sleep 0.5
-          break
-        elsif answer == 'n'
-          puts 'This is a very beautiful city, maybe choose "y"?'
-        else
-          puts 'It seems you work too hard, take care of yourself'
-        end
+
+      if answer == 'y'
+
+        puts '************************************************************************************************'
+
+        puts '                                         "Let\'s start!"                                        '
+
+        puts '************************************************************************************************'
+
+        puts
+
+        sleep 0.5
+
+        break
+
+      elsif answer == 'n'
+
+        puts 'This is a very beautiful city, maybe choose "y"?'
+
+      else
+
+        puts 'It seems you work too hard, take care of yourself'
+
+      end
     end
-      pp scrape_showplaces
-      puts
-      puts
-      puts
-      puts '**************************************"Good luck!"**********************************************'
+
+    pp scrape_showplaces
+
+    puts
+
+    puts
+
+    puts
+
+    puts '**************************************"Good luck!"**********************************************'
   end
 end
 
 scraper = Start.new
-scraper.welcome
-scraper.user_answer
-scraper.scrape_showplaces
-scraper.export_to_csv
 
+scraper.welcome
+
+scraper.user_answer
+
+scraper.scrape_showplaces
+
+scraper.export_to_csv
